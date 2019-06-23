8c30857233024d2f14535ce51701503f90e49683
fechuse <sistemas@glifo.mx>
2019-06-22 19:41:04 -0500

    Revert "Errores PHP"
    This reverts commit c9e54ef0e56fcb0047f419ef6915abc5c8816b41.

--- login/logout.php
+++
@@ -1,12 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-//unset(session_id());
-header("location:main_login.php");
-?>
-<!--
-<SCRIPT LANGUAGE="javascript">
-location.href = "index.php";
-</SCRIPT>
-
--->
