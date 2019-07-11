573eefb312e5c03a0d33a427fbe1df2ddf1c6917
gm-david <cuenta.rsd.job@gmail.com>
2019-07-11 13:40:45 -0500

    Tarjeta Productos: Plantilla [v.1]
    - Nueva Imagen: "logo_mod.jpg"
    - Nuevo PHP: "tarjeta_productos.php"
    - Nuevo CSS: "tarjeta_productos.css"

---
+++ impresion/tarjeta_productos.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tarjeta Información Producto</title>
+
+    <!-- Bootstrap & CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+    <link rel="stylesheet" href="../css/tarjeta_productos.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="row justify-content-between">
+                    <div class="col-6 border border-right-0 border-danger pr-0 pl-0">
+                        <img src="../img/logo_mod.jpg" alt="">
+                    </div>
+        
+                    <div class="col-6 border border-left-0 border-danger pr-4 pl-4 pt-2">
+                        <div class="row h-row-100 align-items-center">
+                            <div class="col-12 h-row">
+                                <div class="rosa row h-row-100 border-pink align-items-center">
+                                    <div class="rosa col-12 text-center"><strong>GOMITAS ENCHILADAS</strong></div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">100 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$5.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">-500 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$50.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">+500 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$550.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="row justify-content-between">
+                    <div class="col-6 border border-right-0 border-danger pr-0 pl-0">
+                        <img src="../img/logo_mod.jpg" alt="">
+                    </div>
+        
+                    <div class="col-6 border border-left-0 border-danger pr-4 pl-4 pt-2">
+                        <div class="row h-row-100 align-items-center">
+                            <div class="col-12 h-row">
+                                <div class="rosa row h-row-100 border-pink align-items-center">
+                                    <div class="rosa col-12 text-center"><strong>GOMITAS ENCHILADAS</strong></div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">100 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$5.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">-500 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$50.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 h-row">
+                                <div class="row h-row-100 align-items-center text-center">
+                                    <div class="col-7 h-row-55 ">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-bold">+500 GRS</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 h-row-55 text-center border-brown">
+                                        <div class="row h-row-100 align-items-center">
+                                            <div class="col-12 text-brown">$550.00</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
