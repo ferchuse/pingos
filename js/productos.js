ead5e4dc2c173a6b04d0f367e9ae9a57be411a08
gm-david <cuenta.rsd.job@gmail.com>
2019-07-30 17:26:38 -0500

    Productos Versión Móvil

--- js/productos.js
+++ js/productos.js
@@ -9,59 +9,103 @@ function listaProductos() {
 		url: 'productos/productos2.php',
 		dataType: 'JSON',
 		data: $("#form_filtros").serializeArray()
-		}).done(function (respuesta) {
+	}).done(function (respuesta) {
 		$.each(respuesta, function (index, value) {
 			bgClass = Number(value.existencia_productos) < Number(value.min_productos) ? "bg-danger" : " ";
-			
+
 			tableTemplate += `
-			<tr class="${bgClass}">
-			<td class="text-center">${value.descripcion_productos}</td>
-			<td class="text-center">${value.nombre_departamentos} </td>
-			<td class="text-center">${value.costo_proveedor} </td>
-			<td class="text-center">${value.ganancia_menudeo_porc} </td>
-			<td class="text-center">${value.precio_menudeo} </td>
-			<td class="text-center">${value.precio_mayoreo} </td>                
-			<td class="text-center">${value.min_productos} </td>
-			<td class="text-center">${value.existencia_productos} </td>                
-			<td class="text-center">
-			<input form='form_imprimir_precios' name="id_productos[]" class="seleccionar" type="checkbox" value="${value.id_productos}">
-			<button class="btn btn-warning btn_editar" data-id_producto="${value.id_productos}">
-			<i class="fa fa-edit"></i>
-			</button>
-			<button class="btn btn-danger btn_eliminar" data-id_producto="${value.id_productos}">
-			<i class="fa fa-trash"></i>
-			</button>
-			<button class="btn btn-success btn_carrito" 
-			data-id_productos="${value.id_productos}"
-			data-descripcion="${value.descripcion_productos}"
-			data-precio="${value.costo_proveedor}"
-			data-unidad="${value.unidad_productos}"
-			>
-			<i class="fa fa-cart-plus"></i>
-			</button>
-			</td>
-			</tr>  
+		<div class="row ${bgClass} mb-3">
+			<div class="col-md-3">
+				<div class="row visible-sm visible-md visible-lg visible-xl">
+					<div class="col-md-12 text-center">${value.descripcion_productos}</div>
+				</div>
+				<div class="row visible-xs bg-primary">
+					<div class="col-xs-12 text-center">${value.descripcion_productos}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Departamento</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.nombre_departamentos}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Costo de Compra</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.costo_proveedor}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Ganancia</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.ganancia_menudeo_porc}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Precio Venta</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.precio_menudeo}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Precio Mayoreo</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.precio_mayoreo}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Mínimo</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.min_productos}</div>
+				</div>
+			</div>
+			<div class="col-md-1">
+				<div class="row">
+					<div class="col-xs-5 text-center visible-xs font-weight-bolder">Existencias</div>
+					<div class="col-xs-7 col-md-1 text-center">${value.existencia_productos}</div>
+				</div>
+			</div>
+			<div class="col-md-2 visible-sm visible-md visible-lg visible-xl">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<input form='form_imprimir_precios' name="id_productos[]" class="seleccionar" type="checkbox"
+							value="${value.id_productos}">
+						<button class="btn btn-warning btn_editar" data-id_producto="${value.id_productos}">
+							<i class="fa fa-edit"></i>
+						</button>
+						<button class="btn btn-danger btn_eliminar" data-id_producto="${value.id_productos}">
+							<i class="fa fa-trash"></i>
+						</button>
+						<button class="btn btn-success btn_carrito" data-id_productos="${value.id_productos}"
+							data-descripcion="${value.descripcion_productos}" data-precio="${value.costo_proveedor}"
+							data-unidad="${value.unidad_productos}">
+							<i class="fa fa-cart-plus"></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>  
 			`;
-			
+
 		});
-		
+
 		$('#bodyProductos').html(tableTemplate);
 		$("#tabla_productos").stickyTableHeaders();
 		$boton.prop("disabled", false)
 		$icono.toggleClass("fa-search fa-spinner fa-spin");
-		
+
 		$(".seleccionar").change(contarSeleccionados)
-		
-		$(".buscar_codigo").keyup( buscarCodigo);
-		$(".buscar_descripcion").keyup( buscarDescripcion);
-		$('.btn_eliminar').click( confirmaEliminar);
-		$('.btn_editar').click( cargarRegistro);
+
+		$(".buscar_codigo").keyup(buscarCodigo);
+		$(".buscar_descripcion").keyup(buscarDescripcion);
+		$('.btn_eliminar').click(confirmaEliminar);
+		$('.btn_editar').click(cargarRegistro);
 		$('.btn_carrito').click(pedirCantidad);
-		
-		
-		}).fail(function(xhr, error, ernum){
+
+
+	}).fail(function (xhr, error, ernum) {
 		alertify.error("Ocurrio un Error" + errnum);
-		
+
 	});
 }
 function buscarCodigo() {
@@ -70,7 +114,7 @@ function buscarCodigo() {
 	var num_rows = buscar(valor_filtro, 'tabla_productos', indice);
 	if (num_rows == 0) {
 		$('#mensaje').html("<div class='alert alert-warning text-center'><strong>No se ha encontrado.</strong></div>");
-		} else {
+	} else {
 		$('#mensaje').html('');
 	}
 }
@@ -82,7 +126,7 @@ function buscarDescripcion() {
 	var num_rows = buscar(valor_filtro, 'tabla_productos', indice);
 	if (num_rows == 0) {
 		$('#mensaje').html("<div class='alert alert-warning text-center'><strong>No se ha encontrado.</strong></div>");
-		} else {
+	} else {
 		$('#mensaje').html('');
 	}
 }
@@ -93,167 +137,167 @@ $(document).ready(function () {
 		listaProductos();
 		return false;
 	})
-	
-	
-	
+
+
+
 	listaProductos();
-	
+
 	$('#btn_alta').click(function () {
 		$('#form_productos')[0].reset();
 		$('h3.modal-title').text('Nuevo Producto');
 		$('#modal_productos').modal('show');
 	});
 	//--------CHECAR DUPLICADOS------
-	$('#codigo_productos').keyup(buscarRepetidos );
-	
-	
-	
-	
+	$('#codigo_productos').keyup(buscarRepetidos);
+
+
+
+
 	//-------ALTA DE PRODUCTOS-----
-	$('#form_productos').submit( guardarProducto);
-	
+	$('#form_productos').submit(guardarProducto);
+
 	//CANTIDAD CONTENEDORA
 	$('#cantidad_contenedora').keyup(function () {
 		var cantidad_contenedora = Number($(this).val());
 		var costo_proveedor = Number($('#costo_proveedor').val());
-		
+
 		if (costo_proveedor != '') {
 			var costo_pz = costo_proveedor / cantidad_contenedora;
 			$('#costo_unitario').val(costo_pz.toFixed(2));
-			
+
 			if (costo_pz != '') {
-				
+
 				//ganancia menudeo
 				var ganancia_menudeo_porc = Number($('#ganancia_menudeo_porc').val());
 				var ganancia_menudeo_pesos = (ganancia_menudeo_porc * costo_pz) / 100;
 				$('#ganancia_menudeo_pesos').val(ganancia_menudeo_pesos.toFixed(2));
-				
+
 				//precio mayoreo
 				var precio_menudeo = costo_pz + ganancia_menudeo_pesos;
 				$('#precio_menudeo').val(precio_menudeo.toFixed(2));
-				
+
 			}
 		}
 	});
-	
+
 	//COSTO PROVEEDOR
 	$('#costo_proveedor').keyup(function modificarPrecio() {
 		console.log("modificarPrecio");
 		var costo_proveedor = Number($(this).val());
 		var cantidad_contenedora = Number($('#cantidad_contenedora').val());
 		var ganancia_mayoreo_porc = Number($('#ganancia_mayoreo_porc').val());
-		
+
 		if (ganancia_mayoreo_porc != '') {
-			
+
 			//ganancia mayoreo
 			var ganancia_mayoreo_pesos = (ganancia_mayoreo_porc * costo_proveedor) / 100;
 			$('#ganancia_mayoreo_pesos').val(ganancia_mayoreo_pesos.toFixed(2));
 			// $('#precio_mayoreo').val((costo_proveedor+ganancia_mayoreo_pesos).toFixed(2));
 		}
-		
+
 		if (cantidad_contenedora != '') {
 			var costo_pz = costo_proveedor / cantidad_contenedora;
 			$('#costo_unitario').val(costo_pz.toFixed(2));
-			
+
 			if (costo_pz != '') {
-				
+
 				//ganancia menudeo
 				var ganancia_menudeo_porc = Number($('#ganancia_menudeo_porc').val());
 				var ganancia_menudeo_pesos = (ganancia_menudeo_porc * costo_pz) / 100;
 				$('#ganancia_menudeo_pesos').val(ganancia_menudeo_pesos.toFixed(2));
-				
+
 				//precio mayoreo
 				var precio_menudeo = costo_pz + ganancia_menudeo_pesos;
 				$('#precio_menudeo').val(precio_menudeo.toFixed(2));
-				
+
 			}
 		}
 	});
-	
+
 	//GANANCIA MAYOREO PORCENTAJE
 	$('#ganancia_mayoreo_porc').keyup(function () {
 		var ganancia_mayoreo_porc = Number($(this).val());
 		var costo_proveedor = Number($('#costo_proveedor').val());
-		
+
 		if (costo_proveedor != '') {
 			var ganancia_mayoreo_pesos = (ganancia_mayoreo_porc * costo_proveedor) / 100;
 			$('#ganancia_mayoreo_pesos').val(ganancia_mayoreo_pesos.toFixed(2));
 			var precio_mayoreo = ganancia_mayoreo_pesos + costo_proveedor;
 			$('#precio_mayoreo').val(precio_mayoreo.toFixed(2));
 		}
-		
+
 	});
-	
+
 	//PRECIO MAYOREO
 	$('#precio_mayoreo').keyup(function () {
 		var precio_mayoreo = Number($(this).val());
 		var costo_proveedor = Number($('#costo_proveedor').val());
-		
+
 		if (costo_proveedor != '') {
 			var ganancia_mayoreo_pesos = precio_mayoreo - costo_proveedor;
 			$('#ganancia_mayoreo_pesos').val(ganancia_mayoreo_pesos.toFixed(2));
-			
+
 			var ganancia_mayoreo_porc = ((precio_mayoreo * 100) / costo_proveedor) - 100;
 			$('#ganancia_mayoreo_porc').val(ganancia_mayoreo_porc.toFixed(2));
 		}
 	});
-	
+
 	//GANANCIA MENUDEO PORCENTAJE
 	$('#ganancia_menudeo_porc').keyup(function () {
 		console.log("calculaPrecioVenta");
-		
+
 		var ganancia_menudeo_porc = Number($(this).val());
 		// var costo_unitario = Number($('#costo_unitario').val());
 		var costo_unitario = Number($('#costo_proveedor').val());
-		
+
 		if (costo_unitario != '') {
 			var ganancia_menudeo_pesos = (ganancia_menudeo_porc * costo_unitario) / 100;
 			$('#ganancia_menudeo_pesos').val(ganancia_menudeo_pesos.toFixed(2));
 			var precio_menudeo = costo_unitario + ganancia_menudeo_pesos;
 			$('#precio_menudeo').val(precio_menudeo.toFixed(2));
 		}
-		
+
 	});
-	
+
 	//PRECIO MENUDEO
 	$('#precio_menudeo').keyup(function calculaGanancia() {
 		console.log("calculaGanancia()")
 		var precio_menudeo = Number($(this).val());
 		var costo_unitario = Number($('#costo_proveedor').val());
-		
+
 		if (costo_unitario != '') {
 			var ganancia_menudeo_porc = ((precio_menudeo * 100) / costo_unitario) - 100;
 			$('#ganancia_menudeo_porc').val(ganancia_menudeo_porc.toFixed(2));
 			var ganancia_menudeo_pesos = precio_menudeo - costo_unitario;
 			$('#ganancia_menudeo_pesos').val(ganancia_menudeo_pesos.toFixed(2));
-			
+
 		}
 	});
-	
+
 	//EXISTENCIA EN CAJAS/PZ
 	$('#existentes_contenedores').keyup(function () {
 		var existentes_contenedores = Number($(this).val());
 		var cantidad_contenedora = Number($('#cantidad_contenedora').val());
-		
+
 		if (cantidad_contenedora != '') {
 			var existencia_productos = existentes_contenedores * cantidad_contenedora;
 			$('#existencia_productos').val(existencia_productos.toFixed(2));
 		}
 	});
-	
+
 	$('#existentes_contenedores_inv').keyup(function () {
 		var existentes_contenedores = Number($(this).val());
 		var cantidad_contenedora = Number($('#cantidad_contenedora').val());
-		
+
 		if (cantidad_contenedora != '') {
 			var existencia_productos = existentes_contenedores * cantidad_contenedora;
 			$('#existencia_productos_inv').val(existencia_productos.toFixed(2));
 		}
 	});
-	
-	
-	
-	
+
+
+
+
 });
 
 
@@ -273,16 +317,16 @@ function cargarRegistro() {
 		method: 'POST',
 		dataType: 'JSON',
 		data: { campo: 'id_productos', tabla: 'productos', id_campo: id_productos }
-		}).done(function (respuesta) {
+	}).done(function (respuesta) {
 		if (respuesta.encontrado == 1) {
 			$.each(respuesta["fila"], function (name, value) {
-				
+
 				$('#input_granel').html('');
 				$.each(respuesta["fila"], function (name, value) {
-					
+
 					$("#form_productos").find("#" + name).val(value);
 				});
-				
+
 			});
 			$('h3.modal-title').text('Editar Producto');
 			$('#modal_productos').modal('show');
@@ -290,7 +334,7 @@ function cargarRegistro() {
 		icono.toggleClass('fa-pencil fa-spinner fa-spin');
 		boton.prop('disabled', false);
 	});
-	
+
 }
 
 function buscar(filtro, table_id, indice) {
@@ -299,14 +343,14 @@ function buscar(filtro, table_id, indice) {
 	filter = filtro.toUpperCase();
 	table = document.getElementById(table_id);
 	tr = table.getElementsByTagName("tr");
-	
+
 	// Loop through all table rows, and hide those who don't match the search query
 	for (i = 0; i < tr.length; i++) {
 		td = tr[i].getElementsByTagName("td")[indice];
 		if (td) {
 			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 				tr[i].style.display = "";
-				} else {
+			} else {
 				tr[i].style.display = "none";
 			}
 		}
@@ -315,8 +359,8 @@ function buscar(filtro, table_id, indice) {
 	return num_rows;
 }
 
-function contarSeleccionados(){
-	console.log( $(".seleccionar:checked"));
+function contarSeleccionados() {
+	console.log($(".seleccionar:checked"));
 	$("#cant_seleccionados").text($(".seleccionar:checked").length);
 }
 
@@ -326,7 +370,7 @@ function guardarProducto(event) {
 	var icono = boton.find('.fa');
 	boton.prop('disabled', true);
 	icono.toggleClass('fa-save fa-spinner fa-spin');
-	
+
 	var formulario = $(this).serializeArray();
 	console.log("formulario: ", formulario)
 	$.ajax({
@@ -334,24 +378,24 @@ function guardarProducto(event) {
 		dataType: 'JSON',
 		method: 'POST',
 		data: formulario
-		}).done(function (respuesta) {
+	}).done(function (respuesta) {
 		console.log(respuesta);
 		if (respuesta.estatus == "success") {
 			alertify.success('Se ha guardado correctamente');
 			$('#modal_productos').modal('hide');
 			listaProductos();
-			} else {
+		} else {
 			alertify.error('Error al guardar');
 			//console.log(respuesta.mensaje);
 		}
-		}).always(function () {
+	}).always(function () {
 		boton.prop('disabled', false);
 		icono.toggleClass('fa-save fa-spinner fa-spin');
 	});
-	
+
 }
 function confirmaEliminar() {
-	
+
 	var boton = $(this);
 	var icono = boton.find('.fa');
 	var fila = boton.closest('tr');
@@ -364,14 +408,14 @@ function confirmaEliminar() {
 			method: 'POST',
 			dataType: 'JSON',
 			data: { tabla: 'productos', campo: 'id_productos', id_campo: id_producto }
-			}).done(function (respuesta) {
+		}).done(function (respuesta) {
 			if (respuesta.estatus == 'success') {
 				alertify.success('Se ha eliminado correctamente');
 				fila.fadeOut(2000);
-				} else {
+			} else {
 				alertify.error('No se ha podido eliminar');
 			}
-			}).always(function () {
+		}).always(function () {
 			boton.prop('disabled', false);
 			icono.toggleClass('fa-trash fa-spinner fa-spin fa-floppy-o');
 		});
@@ -389,11 +433,11 @@ function buscarRepetidos() {
 		method: 'POST',
 		dataType: 'JSON',
 		data: { producto: producto }
-		}).done(function (respuesta) {
+	}).done(function (respuesta) {
 		if (respuesta.repetidos > 0) {
 			$('#btn_formAlta').prop('disabled', true);
 			$('#respuesta_rep').text('(Existentente)');
-			} else {
+		} else {
 			$('#btn_formAlta').prop('disabled', false);
 			$('#respuesta_rep').text('');
 		}
--- productos.php
+++ productos.php
@@ -1,115 +1,112 @@
 <?php
-	include("login/login_success.php");
-	include("conexi.php");
-	include("funciones/generar_select.php");
-	$link = Conectarse();
-	$menu_activo = "producto";
+include("login/login_success.php");
+include("conexi.php");
+include("funciones/generar_select.php");
+$link = Conectarse();
+$menu_activo = "producto";
 ?>
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<style>
-			#respuesta_rep{
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		#respuesta_rep {
 			color: red;
-			}
-		</style>
-    <title>Productos</title>
-		
-		<?php include("styles.php");?>
-		
-	</head>
-  <body>
-		
-		<?php include("menu.php");?>
-		
-		
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<h2 class="text-center">Productos</h2>
-					<hr>
+		}
+		h-auto {
+			height: auto;
+		}
+	</style>
+	<title>Productos</title>
+	<?php include("styles.php"); ?>
+	<link href="css/margenes.css" rel="stylesheet" >
+
+</head>
+
+<body>
+
+	<?php include("menu.php"); ?>
+
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="text-center">Productos</h2>
+				<hr>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<form id="form_filtros" class="form-inline">
+					<div class="form-group">
+						<label for="fecha_inicio">Departamento:</label>
+						<?php echo generar_select($link, "departamentos", "id_departamentos", "nombre_departamentos", true) ?>
+					</div>
+					<div class="form-group">
+						<label for="fecha_fin">Existencias:</label>
+						<select class="form-control" name="existencia">
+							<option value="">TODAS</option>
+							<option value="minimo">DEBAJO DEL MINIMO</option>
+						</select>
+					</div>
+
+					<button type="submit" class="btn btn-primary" id="btn_buscar">
+						<i class="fa fa-search"></i> Buscar
+					</button>
+				</form>
+				<button type="button" class="btn btn-success pull-right visible-sm visible-md visible-lg visible-xl" id="btn_alta">
+					<i class="fa fa-plus"></i> Nuevo
+				</button>
+				<button type="submit" form="form_imprimir_precios" class="btn btn-info pull-right visible-sm visible-md visible-lg visible-xl" form="btn_imprimir_precios">
+					<i class="fa fa-print"></i> Imprimir Precios
+					(<span id="cant_seleccionados">0</span>)
+				</button>
+
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-md-12 text-center table-responsive table-bordered visible-sm visible-md visible-lg visible-xl" id="lista_productos">
+				<div class="row bg-primary">
+					<div class="col-md-3 text-center font-weight-bolder">Descripción</div>
+					<div class="col-md-1 text-center font-weight-bolder">Departamento</div>
+					<div class="col-md-1 text-center font-weight-bolder">Costo de Compra</div>
+					<div class="col-md-1 text-center font-weight-bolder">Ganancia</div>
+					<div class="col-md-1 text-center font-weight-bolder">Precio Venta</div>
+					<div class="col-md-1 text-center font-weight-bolder">Precio Mayoreo</div>
+					<div class="col-md-1 text-center font-weight-bolder">Mínimo</div>
+					<div class="col-md-1 text-center font-weight-bolder">Existencia</div>
+					<div class="col-md-2 text-center font-weight-bolder">Acciones</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-3 text-center mt-3 mb-3">
+						<input type="text" class="form-control buscar_descripcion" data-indice="0" placeholder="Buscar Descripción">
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<form id="form_filtros" class="form-inline">
-						<div class="form-group">
-							<label for="fecha_inicio">Departamento:</label>
-							<?php echo generar_select($link, "departamentos", "id_departamentos", "nombre_departamentos", true)?>
-						</div>
-						<div class="form-group">
-							<label for="fecha_fin">Existencias:</label>
-							<select  class="form-control"  name="existencia">
-								<option value="">TODAS</option>
-								<option value="minimo">DEBAJO DEL MINIMO</option>
-							</select>
-						</div>
-						
-						<button type="submit" class="btn btn-primary" id="btn_buscar">
-							<i class="fa fa-search"></i> Buscar
-						</button>
-					</form>
-					<button type="button" class="btn btn-success pull-right" id="btn_alta">
-						<i class="fa fa-plus"></i> Nuevo
-					</button>
-					<button type="submit" form="form_imprimir_precios" class="btn btn-info pull-right" form="btn_imprimir_precios">
-						<i class="fa fa-print"></i> Imprimir Precios  
-						(<span id="cant_seleccionados">0</span>)
-					</button>
-					
-				</div>
-			</div>
-			<br>
-			<div class="row">
-				<div class="col-md-12 text-center table-responsive" id="lista_productos">
-					
-					<table class="table table-bordered" id="tabla_productos">
-						<thead class="bg-primary">
-							<tr>
-								<th class="text-center">Descripción</th>
-								<th class="text-center">Departamento</th>
-								<th class="text-center">Costo de Compra</th>
-								<th class="text-center">Ganancia</th>
-								<th class="text-center">Precio Venta</th>
-								<th class="text-center">Precio Mayoreo</th>
-								<th class="text-center">Mínimo</th>
-								<th class="text-center">Existencia</th>
-								<th class="text-center">Acciones</th>	
-							</tr>
-							<tr>
-								<th class="text-center">
-									<input type="text" class="form-control buscar_descripcion" data-indice="0" placeholder="Buscar descripcion">
-								</th>
-								<th colspan="8">
-								</th>
-							</tr>
-						</thead>
-						
-						<tbody id="bodyProductos">                    
-							<tr>
-								<th class="text-center" colspan="9">
-									<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-								</th>
-							</tr>                   
-							
-						</tbody>
-						
-					</table>
-					
+
+			<div class="col-md-12" id="bodyProductos">
+				<div class="row">
+					<div class="col text-center" colspan="9">
+						<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+					</div>
 				</div>
 			</div>
 		</div>
-		<form id="form_imprimir_precios" action="impresion/imprimir_precios.php">
-		</form>
-		<?php include('forms/productos.php'); ?>
-		<?php include('forms/existencias.php'); ?>
-		
-		<?php  include('scripts.php'); ?>
-		<script src="js/productos.js"></script>
-		<script src="js/carrito.js"></script>
-		<script src="https://unpkg.com/sticky-table-headers"></script>
-	</body>
+	</div>
+	<form id="form_imprimir_precios" action="impresion/imprimir_precios.php">
+	</form>
+	<?php include('forms/productos.php'); ?>
+	<?php include('forms/existencias.php'); ?>
+
+	<?php include('scripts.php'); ?>
+	<script src="js/productos.js"></script>
+	<script src="js/carrito.js"></script>
+	<script src="https://unpkg.com/sticky-table-headers"></script>
+</body>
+
 </html>
