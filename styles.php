0bfe8fa15f2f304d35dab337028d2aaf11565a69
ferchuse <sistemas@glifo.mx>
2020-02-24 10:29:05 -0600

    Update styles.php

--- styles.php
+++ styles.php
@@ -1,6 +1,6 @@
 <link href="css/bootstrap.min.css" rel="stylesheet" media="all">
 <link href="css/alertify.min.css" rel="stylesheet" media="all"/>
-<link href="css/common.css" rel="stylesheet" media="all"  >
+<link href="css/common.css?<?= date("Y-m-d-H-i-s")?>" rel="stylesheet" media="all"  >
 <link href="css/imprimir_venta.css" rel="stylesheet" media="all">
 <link href="css/all.min.css" rel="stylesheet" >
 <link href="lib/autocomplete.css" rel="stylesheet" >
